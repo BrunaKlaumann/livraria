5511b58a3bb31a35d60e516ccc7c5128c71b0b74
Matheus Miliorini <miliorinimatheus@gmail.com>
2019-08-22 21:45:57 -0300

    Projeto .net

---
+++ Livraria/Livraria/Controllers/Conexao.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.Controllers
+{
+    public class Conexao
+    {
+        public static NpgsqlConnection getConexao()
+        {
+            NpgsqlConnection conexao = null;
+            try
+            {
+                conexao = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=postgres;Database=livraria");
+                conexao.Open();
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro de conexão: " + erro.Message);
+            }
+            return conexao;
+        }
+
+        public static void setFechaConexao(NpgsqlConnection conexao)
+        {
+            if (conexao != null)
+            {
+                try
+                {
+                    conexao.Close();
+                }
+                catch (Exception erro)
+                {
+                    Console.WriteLine("Erro ao fechar conexão: " + erro.Message);
+                }
+            }
+        }
+    }
+}
