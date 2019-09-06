60957a6fd07774be6f2012e2f60ec6c422d04ea6
Bruna Klaumann <39775548+BrunaKlaumann@users.noreply.github.com>
2019-09-05 21:44:19 -0300

    AutorLivro [Ajuda pls Matheus]

--- Livraria/Livraria/Controllers/AutorLivroController.cs
+++ Livraria/Livraria/Controllers/AutorLivroController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Livraria.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,43 @@ namespace Livraria.Controllers
     [ApiController]
     public class AutorLivroController : ControllerBase
     {
-    }
+        public static List<AutorLivro> lista = new List<AutorLivro>();
+
+        [AcceptVerbs("GET")]
+        public List<AutorLivro> GetAutorLivro()
+        {
+            return lista;
+        }
+
+        [AcceptVerbs("POST")]
+        public string PostAutorLivro(AutorLivro autorLivro)
+        {
+            lista.Add(autorLivro);
+            return "Autor incluído com Sucesso!";
+        }
+        [AcceptVerbs("PUT")]
+        public string PutAutorLivro(AutorLivro autorLivro)
+            //Mexi ate aqui apenas para baixo s'o ctrl c
+        {
+            lista.Where(l => l.id_autor == autores.id_autor)
+                .Select(o => {
+                    o.nome = autores.nome;
+                    o.id_autor = autores.id_autor;
+                    return o;
+                })
+                .ToList();
+            return " Autor alterado com Sucesso!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        public string DeleteAutores(Autores autores)
+        {
+            Autores auxautores = lista.Where(l => l.id_autor == autores.id_autor)
+                    .Select(o => o)
+                    .First();
+            lista.Remove(auxautores);
+            return "Autor excluido com sucesso!";
+        }
+    }2
+}
 }
--- Livraria/Livraria/Controllers/AutorLivroDB.cs
+++ Livraria/Livraria/Controllers/AutorLivroDB.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Livraria.Models;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,103 @@ namespace Livraria.Controllers
 {
     public class AutorLivroDB
     {
+        public static List<AutorLivro> GetAutorLivro()
+        {
+            List<AutorLivro> lista = new List<AutorLivro>();
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "select * from AutorLivro";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    int codigoLivro = (int)dr["id_livro"];
+                    int codigoAutor = (int)dr["id_autor"];
+                    AutorLivro autorLivro = new AutorLivro();
+                    autorLivro.id_autor = codigoLivro;
+                    autorLivro.id_autor = codigoAutor;
+                    lista.Add(autorLivro);
+
+                }
+            }
+            //Fiz ate aqui o resto para baixo nao fiz nada 
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao consultar Cidade." + erro.Message);
+            }
+            return lista;
+        }
+        public static bool SetIncuiCidade(Cidade cidade)
+        {
+            bool incluiu = false;
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "insert into cidade(cid_codigo,nome,est_sigla) values(@codigo,@nome,@sigla)";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.cid_codigo;
+                cmd.Parameters.Add("@nome", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.nome;
+                cmd.Parameters.Add("@sigla", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.est_sigla;
+
+                int valor = cmd.ExecuteNonQuery();
+                if (valor == 1)
+                {
+                    incluiu = true;
+                }
+
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro inclusão da cidade " + erro.Message);
+            }
+            return incluiu;
+        }
+        public static bool SetAlteraCidade(Cidade cidade)
+        {
+            bool alterou = false;
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "update cidade set nome=@nome where cid_codigo = @codigo and est_sigla = @sigla";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.cid_codigo;
+                cmd.Parameters.Add("@nome", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.nome;
+                cmd.Parameters.Add("@sigla", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.est_sigla;
+                int valor = cmd.ExecuteNonQuery();
+                if (valor == 1)
+                {
+                    alterou = true;
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao alterar Cidade" + erro.Message);
+
+            }
+            return alterou;
+        }
+        public static bool SetExcluiCidade(Cidade cidade)
+        {
+            bool excluiu = false;
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "delete from cidade where cid_codigo = @codigo";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = cidade.cid_codigo;
+                int valor = cmd.ExecuteNonQuery();
+                if (valor == 1)
+                {
+                    excluiu = true;
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao excluir Cidade" + erro.Message);
+            }
+            return excluiu;
+        }
+
     }
 }
