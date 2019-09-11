e9bd8aaa56a9b94538790a27c1db6cc6d06417f3
Matheus Miliorini <miliorinimatheus@gmail.com>
2019-09-11 19:19:13 -0300

    Usuario OK

--- Livraria/Livraria/Controllers/UsuarioDB.cs
+++ Livraria/Livraria/Controllers/UsuarioDB.cs
@@ -7,99 +7,104 @@ using System.Threading.Tasks;
 
 namespace Livraria.Controllers
 {
-//    public class UsuarioDB
-//    {
-//        List<Autores> lista = new List<Autores>();
-//            try
-//            {
-//                NpgsqlConnection conexao = Conexao.GetConexao();
-//                string sql = "select * from autores";
-//                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
-//                NpgsqlDataReader dr = cmd.ExecuteReader();
-//                while (dr.Read())
-//                {
-//                    int codigo = (int)dr["id_autor"];
-//        string nome = (string)dr["nome"];
-//        Autores autores = new Autores();
-//        autores.id_autor = codigo;
-//                        autores.nome = nome;
-//                        lista.Add(autores);
+    public class UsuarioDB
+    {
+        public static List<Usuario> GetUsuarios()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "SELECT * FROM usuarios";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.id_usuario = (int)dr["id_usuario"];
+                    usuario.nome = (string)dr["nome"];
+                    lista.Add(usuario);
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao consultar usuarios." + erro.Message);
+            }
+            return lista;
+        }
 
-//                }
-//}
-//            catch (NpgsqlException erro)
-//            {
-//                Console.WriteLine("Erro ao consultar Autores." + erro.Message);
-//            }
-//            return lista;
-//        }
-//        public static bool SetIncuiAutores(Autores autores)
-//{
-//    bool incluiu = false;
-//    try
-//    {
-//        NpgsqlConnection conexao = Conexao.GetConexao();
-//        string sql = "insert into autores(id_autor,nome) values(@codigo,@nome)";
-//        NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
-//        cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = autores.id_autor;
-//        cmd.Parameters.Add("@nome", NpgsqlTypes.NpgsqlDbType.Varchar).Value = autores.nome;
+        public static string insereUsuario(Usuario usuario)
+        {
+            int incluidos = 0;
 
-//        int valor = cmd.ExecuteNonQuery();
-//        if (valor == 1)
-//        {
-//            incluiu = true;
-//        }
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "INSERT INTO USUARIOS(nome) VALUES(@nome)";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.AddWithValue("@nome", usuario.nome);
 
-//    }
-//    catch (NpgsqlException erro)
-//    {
-//        Console.WriteLine("Erro ao incluir Autor" + erro.Message);
-//    }
-//    return incluiu;
-//}
-//public static bool SetAlteraAutores(Autores autores)
-//{
-//    bool alterou = false;
-//    try
-//    {
-//        NpgsqlConnection conexao = Conexao.GetConexao();
-//        string sql = "update autores set nome=@nome where id_autores= @codigo and nome= @nome";
-//        NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
-//        cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = autores.id_autor;
-//        cmd.Parameters.Add("@nome", NpgsqlTypes.NpgsqlDbType.Varchar).Value = autores.nome;
-//        int valor = cmd.ExecuteNonQuery();
-//        if (valor == 1)
-//        {
-//            alterou = true;
-//        }
-//    }
-//    catch (NpgsqlException erro)
-//    {
-//        Console.WriteLine("Erro ao alterar Autor" + erro.Message);
+                incluidos = cmd.ExecuteNonQuery();
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao incluir usuário " + erro.Message);
+            }
 
-//    }
-//    return alterou;
-//}
-//public static bool SetExcluiAutores(Autores autores)
-//{
-//    bool excluiu = false;
-//    try
-//    {
-//        NpgsqlConnection conexao = Conexao.GetConexao();
-//        string sql = "delete from autores where id_autor = @codigo";
-//        NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
-//        cmd.Parameters.Add("@codigo", NpgsqlTypes.NpgsqlDbType.Varchar).Value = autores.id_autor;
-//        int valor = cmd.ExecuteNonQuery();
-//        if (valor == 1)
-//        {
-//            excluiu = true;
-//        }
-//    }
-//    catch (NpgsqlException erro)
-//    {
-//        Console.WriteLine("Erro ao excluir Autor" + erro.Message);
-//    }
-//    return excluiu;
-//}
-//    }
+            if (incluidos == 1)
+            {
+                return "Usuário incuído com sucesso.";
+            }
+            else
+            {
+                return "Falha ao incluir o usuário";
+            }
+        }
+
+        public static bool putUsuario(Usuario usuario)
+        {
+            bool alterou = false;
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "UPDATE usuarios SET nome = @nome WHERE id_usuario = @codigo";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.AddWithValue("@codigo", usuario.id_usuario);
+                cmd.Parameters.AddWithValue("@nome", usuario.nome);
+                int valor = cmd.ExecuteNonQuery();
+                if (valor == 1)
+                {
+                    alterou = true;
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao alterar Usuário " + erro.Message);
+            }
+            return alterou;
+        }
+
+        public static bool deleteUsuario(Usuario usuario)
+        {
+            bool excluiu = false;
+
+            try
+            {
+                NpgsqlConnection conexao = Conexao.GetConexao();
+                string sql = "DELETE FROM usuarios WHERE id_usuario = @codigo";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conexao);
+                cmd.Parameters.AddWithValue("@codigo", usuario.id_usuario);
+                int valor = cmd.ExecuteNonQuery();
+                if (valor == 1)
+                {
+                    excluiu = true;
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                Console.WriteLine("Erro ao excluir Usuário " + erro.Message);
+            }
+            return excluiu;
+        }
+    }
 }
---
+++ Livraria/Livraria/Controllers/UsuariosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livraria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livraria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [AcceptVerbs("GET")]
+        public List<Usuario> GetUsuarios()
+        {
+            return UsuarioDB.GetUsuarios();
+        }
+
+        [AcceptVerbs("POST")]
+        public string PostUsuario(Usuario usuario)
+        {
+            return UsuarioDB.insereUsuario(usuario);
+        }
+
+        [AcceptVerbs("PUT")]
+        public bool PutUsuario(Usuario usuario)
+        {
+            return UsuarioDB.putUsuario(usuario);
+        }
+
+        [AcceptVerbs("DELETE")]
+        public bool DeleteUsuario(Usuario usuario)
+        {
+            return UsuarioDB.deleteUsuario(usuario);
+        }
+    }
+}
